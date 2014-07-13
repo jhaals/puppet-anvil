75d0ab523964df8de333052e87b1a03a8bcb08c2
Johan Haals <johan.haals@gmail.com>
2014-07-13 23:30:18 +0200

    return empty response if no modules are found

--- main.go
+++ main.go
@@ -29,10 +29,16 @@ func ReleaseHandler(w http.ResponseWriter, r *http.Request) {
 	user := strings.Split(moduleName, "-")[0]
 	mod := strings.Split(moduleName, "-")[1]
 
+	modules := module.ListModules(filepath.Join(moduleDir, user, mod))
+	// No modules, return minimal json response.
+	if len(modules) == 0 {
+		fmt.Fprintf(w, `{"pagination":{"next":null},"results":[]}`)
+		return
+	}
 	response := new(module.Response)
 	response.Pagination = module.Pagination{Next: false}
 
-	for _, file := range module.ListModules(filepath.Join(moduleDir, user, mod)) {
+	for _, file := range modules {
 		metadata, _ := module.ReadMetadata(file + ".metadata")
 		var result = module.Result{
 			Uri:     fmt.Sprintf("/v3/release/%s/%s", metadata.Name, metadata.Version),
