c5fad78a46b757d81d5899ac06c09dc3c114c11e
Johan Haals <johan.haals@gmail.com>
2014-07-15 18:39:06 +0200

    get port and modulepath from environment variables

--- main.go
+++ main.go
@@ -4,22 +4,36 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/jhaals/go-puppet-forge/module"
+	"log"
 	"net/http"
+	"os"
 	"path/filepath"
 	"strings"
 )
 
 func main() {
-	// List all releases for a module
+	port := os.Getenv("PORT")
+	modulePath := os.Getenv("MODULEPATH")
+
+	if len(port) == 0 {
+		log.Fatal("Missing PORT environment variable")
+	}
+	if len(modulePath) == 0 {
+		log.Fatal("Missing MODULEPATH environment variable")
+	}
+
+	log.Println("Starting go-puppet-forge on port", port, "serving modules from", modulePath)
+
 	http.HandleFunc("/v3/files/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "/var/lib/go-puppet-forge/modules/"+r.URL.Path[10:])
+		http.ServeFile(w, r, filepath.Join(modulePath, r.URL.Path[10:]))
 	})
-	http.HandleFunc("/v3/releases", ReleaseHandler)
-	http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/v3/releases", func(w http.ResponseWriter, r *http.Request) {
+		ReleaseHandler(w, r, modulePath)
+	})
+	http.ListenAndServe(":"+port, nil)
 }
 
-func ReleaseHandler(w http.ResponseWriter, r *http.Request) {
-	moduleDir := "/var/lib/go-puppet-forge/modules/"
+func ReleaseHandler(w http.ResponseWriter, r *http.Request, modulePath string) {
 	moduleName := r.URL.Query().Get("module")
 	if !strings.Contains(moduleName, "-") {
 		http.Error(w, "request must be /v3/releases?module=user-module", 400)
@@ -29,7 +43,7 @@ func ReleaseHandler(w http.ResponseWriter, r *http.Request) {
 	user := strings.Split(moduleName, "-")[0]
 	mod := strings.Split(moduleName, "-")[1]
 
-	modules := module.ListModules(filepath.Join(moduleDir, user, mod))
+	modules := module.ListModules(filepath.Join(modulePath, user, mod))
 	// No modules, return minimal json response.
 	if len(modules) == 0 {
 		fmt.Fprintf(w, `{"pagination":{"next":null},"results":[]}`)
