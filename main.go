0e5a2d6d370bdfabc046b17d7b58cc0493e9b46e
Ben Schwartz <benschw@gmail.com>
2015-07-18 15:44:02 -0500

    cleaning up

--- main.go
+++ main.go
@@ -27,54 +27,34 @@ func main() {
 
 	log.Println("Starting Puppet Anvil on port", port, "serving modules from", modulePath)
 
+	forge := &ForgeResource{ModulePath: modulePath}
+	admin := &AdminResource{ModulePath: modulePath}
+
 	mr := mux.NewRouter()
-	mr.HandleFunc("/v3/files/{user}/{module}/{fileName}", func(w http.ResponseWriter, r *http.Request) {
-		log.Println("files")
-
-		user, _ := rest.PathString(r, "user")
-		module, _ := rest.PathString(r, "module")
-		fileName, _ := rest.PathString(r, "fileName")
-
-		http.ServeFile(w, r, filepath.Join(modulePath, user, module, fileName))
-	}).Methods("GET")
-
-	mr.HandleFunc("/v3/releases", func(w http.ResponseWriter, r *http.Request) {
-		log.Println("releases")
-		ReleaseHandler(w, r, modulePath)
-	}).Methods("GET")
-
-	mr.HandleFunc("/modules/{user}/{module}/{fileName}", func(w http.ResponseWriter, r *http.Request) {
-		log.Println("upload")
-		upsertFile(w, r)
-	}).Methods("PUT")
+	mr.HandleFunc("/v3/files/{user}/{module}/{fileName}", forge.GetModule).Methods("GET")
+	mr.HandleFunc("/v3/releases", forge.GetReleases).Methods("GET")
+	mr.HandleFunc("/modules/{user}/{module}/{fileName}", admin.UpsertFile).Methods("PUT")
 
 	http.Handle("/", mr)
 
 	http.ListenAndServe(":"+port, nil)
 }
-func upsertFile(w http.ResponseWriter, r *http.Request) {
-	content, _ := ioutil.ReadAll(r.Body)
-	user, _ := rest.PathString(r, "user")
-	module, _ := rest.PathString(r, "module")
-	fileName, _ := rest.PathString(r, "fileName")
 
-	modulePath := fmt.Sprintf("/modules/%s/%s", user, module)
-	if _, err := os.Stat(modulePath); err != nil {
-		os.MkdirAll(modulePath, 0755)
-	}
+type ForgeResource struct {
+	ModulePath string
+}
 
-	if err := ioutil.WriteFile(modulePath+"/"+fileName, content, 0666); err != nil {
-		log.Fatal(err)
+func (f *ForgeResource) GetModule(w http.ResponseWriter, r *http.Request) {
+	user, module, fileName, err := getModulePathComponents(r)
+	if err != nil {
+		rest.SetBadRequestResponse(w)
 		return
 	}
 
-	url := fmt.Sprintf("/v3/files/%s/%s/%s", user, module, fileName)
-
-	w.Header().Set("Location", url)
-	w.Header().Set("Content-Type", "application/json")
+	http.ServeFile(w, r, filepath.Join(f.ModulePath, user, module, fileName))
 }
 
-func ReleaseHandler(w http.ResponseWriter, r *http.Request, modulePath string) {
+func (f *ForgeResource) GetReleases(w http.ResponseWriter, r *http.Request) {
 	moduleName := r.URL.Query().Get("module")
 	if !strings.Contains(moduleName, "-") {
 		http.Error(w, "request must be /v3/releases?module=user-module", 400)
@@ -84,7 +64,7 @@ func ReleaseHandler(w http.ResponseWriter, r *http.Request, modulePath string) {
 	user := strings.Split(moduleName, "-")[0]
 	mod := strings.Split(moduleName, "-")[1]
 
-	modules := ListModules(filepath.Join(modulePath, user, mod))
+	modules := ListModules(filepath.Join(f.ModulePath, user, mod))
 	// No modules, return minimal json response.
 	if len(modules) == 0 {
 		fmt.Fprintf(w, `{"pagination":{"next":null},"results":[]}`)
@@ -94,7 +74,7 @@ func ReleaseHandler(w http.ResponseWriter, r *http.Request, modulePath string) {
 	response.Pagination = Pagination{Next: false}
 
 	for _, metadata := range modules {
-		checksum, err := Checksum(filepath.Join(modulePath, user, mod, moduleName+"-"+metadata.Version+".tar.gz"))
+		checksum, err := Checksum(filepath.Join(f.ModulePath, user, mod, moduleName+"-"+metadata.Version+".tar.gz"))
 		if err != nil {
 			// Unable to checksum modulefile, log and skip.
 			log.Println(err)
@@ -110,4 +90,48 @@ func ReleaseHandler(w http.ResponseWriter, r *http.Request, modulePath string) {
 	}
 	jsonData, _ := json.Marshal(response)
 	fmt.Fprintf(w, string(jsonData))
+}
+
+type AdminResource struct {
+	ModulePath string
+}
+
+func (a *AdminResource) UpsertFile(w http.ResponseWriter, r *http.Request) {
+	content, _ := ioutil.ReadAll(r.Body)
+	user, module, fileName, err := getModulePathComponents(r)
+	if err != nil {
+		rest.SetBadRequestResponse(w)
+		return
+	}
+
+	fullModulePath := fmt.Sprintf("%s/%s/%s", a.ModulePath, user, module)
+	if _, err := os.Stat(fullModulePath); err != nil {
+		os.MkdirAll(fullModulePath, 0755)
+	}
+
+	if err := ioutil.WriteFile(fullModulePath+"/"+fileName, content, 0666); err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	url := fmt.Sprintf("/v3/files/%s/%s/%s", user, module, fileName)
+
+	w.Header().Set("Location", url)
+	w.Header().Set("Content-Type", "application/json")
+}
+
+func getModulePathComponents(r *http.Request) (string, string, string, error) {
+	user, err := rest.PathString(r, "user")
+	if err != nil {
+		return "", "", "", err
+	}
+	module, err := rest.PathString(r, "module")
+	if err != nil {
+		return "", "", "", err
+	}
+	fileName, err := rest.PathString(r, "fileName")
+	if err != nil {
+		return "", "", "", err
+	}
+	return user, module, fileName, nil
 }
--- test.sh
+++ test.sh
@@ -2,10 +2,10 @@
 
 CWD=$(pwd)
 
-rm -rf out modules
+sudo rm -rf out modules
 mkdir -p out modules/puppetlabs/apache modules/puppetlabs/concat modules/puppetlabs/stdlib
 
-go build -o puppet-anvil
+go build -o puppet-anvil && echo "'puppet-anvil' built"
 
 docker build -t puppet-anvil .
 
@@ -21,9 +21,6 @@ curl -i -X PUT http://localhost:8080/modules/puppetlabs/apache/puppetlabs-apache
 curl -i -X PUT http://localhost:8080/modules/puppetlabs/concat/puppetlabs-concat-1.2.3.tar.gz -T ./puppetlabs-concat-1.2.3.tar.gz
 curl -i -X PUT http://localhost:8080/modules/puppetlabs/stdlib/puppetlabs-stdlib-4.6.0.tar.gz -T ./puppetlabs-stdlib-4.6.0.tar.gz
 
-#wget https://forgeapi.puppetlabs.com/v3/files/puppetlabs-apache-1.5.0.tar.gz -O modules/puppetlabs/apache/puppetlabs-apache-1.5.0.tar.gz
-#wget https://forgeapi.puppetlabs.com/v3/files/puppetlabs-concat-1.2.3.tar.gz -O modules/puppetlabs/concat/puppetlabs-concat-1.2.3.tar.gz
-#wget https://forgeapi.puppetlabs.com/v3/files/puppetlabs-stdlib-4.6.0.tar.gz -O modules/puppetlabs/stdlib/puppetlabs-stdlib-4.6.0.tar.gz
 
 puppet module install puppetlabs/apache --module_repository http://127.0.0.1:8080 --modulepath ./out
 
