c6b64bac6239b1629b3131f7567472439a1365fb
Paul Hamby <phamby@homeaway.com>
2016-10-03 09:03:36 -0500

    Adds support for librarian-puppet

--- api/forge_api.go
+++ api/forge_api.go
@@ -18,6 +18,50 @@ type Result struct {
 	Md5      string `json:"file_md5"`
 	Metadata `json:"metadata"`
 }
+
+type Owner struct {
+	Uri      string `json:"uri"`
+	Slug     string `json:"slug"`
+	Username string `json:"username"`
+}
+
+type CurrentRelease struct {
+	Uri               string `json:"uri"`
+	Slug              string `json:"slug"`
+	ModuleAbbreviated `json:"module"`
+	Metadata          `json:"metadata"`
+}
+
+type ModuleAbbreviated struct {
+	Uri   string `json:"uri"`
+	Slug  string `json:"slug"`
+	Name  string `json:"name"`
+	Owner `json:"owner"`
+}
+
+type ModuleResult struct {
+	Uri            string `json:"uri"`
+	Slug           string `json:"slug"`
+	Name           string `json:"name"`
+	Owner          `json:"owner"`
+	CurrentRelease `json:"current_release"`
+	FileUri        string    `json:"file_uri"`
+	Version        string    `json:"version"`
+	Md5            string    `json:"file_md5"`
+	Releases       []Release `json:"releases"`
+}
+
+type Release struct {
+	Uri       string  `json:"uri"`
+	Slug      string  `json:"slug"`
+	FileUri   string  `json:"file_uri"`
+	Version   string  `json:"version"`
+	Deleted   *string `json:"deleted_at"`
+	Created   *string `json:"created_at"`
+	Supported *string `json:"supported"`
+	FileSize  *string `json:"file_size"`
+}
+
 type Pagination struct {
 	Next bool `json:"next"`
 }
--- service/user_input.go
+++ service/user_input.go
@@ -14,12 +14,26 @@ func parseFileNamePathParam(r *http.Request) (string, string, string, error) {
 	return user, mod, fileName, err
 }
 
+func parseUserModulePathParam(r *http.Request) (string, string, error) {
+	userModule := mux.Vars(r)["user-module"]
+	user, mod, err := parseModuleName(userModule)
+	return user, mod, err
+}
+
 func parseModuleGetParam(r *http.Request) (string, string, error) {
 	moduleName := r.URL.Query().Get("module")
 	if len(moduleName) == 0 {
 		return "", "", fmt.Errorf("GET param 'module' empty")
 	}
 	return parseModuleName(moduleName)
+}
+
+func parseQueryGetParam(r *http.Request) (string, string, error) {
+	queryName := r.URL.Query().Get("query")
+	if len(queryName) == 0 {
+		return "", "", fmt.Errorf("GET param 'query' empty")
+	}
+	return parseModuleName(queryName)
 }
 func parseFileName(fileName string) (string, string, error) {
 	if !strings.HasSuffix(fileName, ".tar.gz") {
@@ -42,4 +56,16 @@ func parseModuleName(moduleName string) (string, string, error) {
 	mod := strings.Split(moduleName, "-")[1]
 
 	return user, mod, nil
+}
+
+func parseReleasePathParam(r *http.Request) (string, string, string, error) {
+	userModuleVersion := mux.Vars(r)["user-module-version"]
+	if strings.Count(userModuleVersion, "-") != 2 {
+		return "", "", "", fmt.Errorf("user-module-version '%s' should be of the form '{user}-{module}-{version}", userModuleVersion)
+	}
+	user := strings.Split(userModuleVersion, "-")[0]
+	mod := strings.Split(userModuleVersion, "-")[1]
+	version := strings.Split(userModuleVersion, "-")[2]
+
+	return user, mod, version, nil
 }
